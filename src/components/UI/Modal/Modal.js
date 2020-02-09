015ee16d3ea716051cfac5261d411b014b34b857
Jose <josepuertas@gmail.com>
2020-02-09 13:59:22 +0100

    Add Modal Component

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import classes from './Modal.module.css';
+import Backdrop from '../Backdrop/Backdrop';
+
+
+const Modal = ({show, modalClosed, children}) => (
+  <>
+  <Backdrop show={show} clicked={modalClosed} />
+  <div
+    className={classes.Modal}
+    style={{
+      transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+      opacity: show ? '1' : '0'
+    }}>
+      {children}
+  </div>
+  </>
+);
+
+export default Modal;
